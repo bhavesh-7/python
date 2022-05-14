b95e5e737b677ccfc07c5fdf13f746104edd6ecd
Bhavesh Mishra <91950960+bhavesh-7@users.noreply.github.com>
2022-05-15 00:50:03 +0530

    Create calculatur.py

---
+++ calculatur.py
@@ -0,0 +1,14 @@
+fnum = int(input("enter first number: "))
+snum = int(input("enter second number: "))
+add = fnum+snum
+sub = fnum-snum
+mul = fnum*snum
+print("Addition       : ",add)
+print("subtraction    : ", sub)
+print("multiplication : ", mul)
+if (snum!=0):
+    div = fnum/snum
+    print("Division       : ", div)
+else:
+    print("Division       : Not Defined")
+    
